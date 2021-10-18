319100a702633ee0809f04803ce9d309ab4b0dc7
Amrithanshu Kesoju <92728684+amrith-anshu@users.noreply.github.com>
2021-10-18 20:31:13 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+declare var jQuery: any;
+
+@Component({
+  selector: 'main-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Calculator-app';
+
+}
+
