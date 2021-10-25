1b015a9e5e765e6ea3b82aa3ab6a8bfba1c906a2
Amrithanshu Kesoju <92728684+amrith-anshu@users.noreply.github.com>
2021-10-25 20:47:35 +0530

    Angular Material Components Update

---
+++ app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+declare var jQuery: any;
+
+@Component({
+  selector: 'main-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Calculator-app';
+
+}
+
+export class ButtonTypesExample { }
